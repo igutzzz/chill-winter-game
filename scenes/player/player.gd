6710338b4e0fc0c3c6bbf67b8fca637b27e3fd9b
Igor de Almeida <ofc.iguituu@gmail.com>
2023-12-03 23:19:54 -0300

    first commit

---
+++ scenes/player/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	
+	if input_vector != Vector2.ZERO:
+		velocity = input_vector
+	else:
+		velocity = Vector2.ZERO
+	
+	move_and_collide(velocity)
