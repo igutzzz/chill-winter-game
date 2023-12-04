11b2acdfa01ff40a5a084aa52b5f3f8571f08ac5
Igor Almeida <igor.almeida@madeinweb.app>
2023-12-04 18:13:48 -0300

    player movement + animation + collision

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,12 @@
 extends CharacterBody2D
 
+const MAX_SPEED = 100
+const ACCELERATION = 500
+const FRICTION = 500
 
+
+@onready var animation_tree = $AnimationTree
+@onready var animation_state = animation_tree.get("parameters/playback")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,10 +18,17 @@ func _process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		animation_tree.set("parameters/idle/blend_position", input_vector)
+		animation_tree.set("parameters/walk/blend_position", input_vector)
+		animation_state.travel("walk")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		velocity = Vector2.ZERO
+		animation_state.travel("idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+
 	
-	move_and_collide(velocity)
+	
+	move_and_slide()
---
+++ scenes/projectiles/snowball.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+@export var SPEED: int = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var direction = get_viewport().get_mouse_position()
+	global_position += SPEED * direction * delta
